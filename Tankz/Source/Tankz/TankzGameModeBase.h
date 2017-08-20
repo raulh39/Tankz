2b538086dcd988aca7679c9efc2cd0cef16f9149
raul.huertas <raulh39@gmail.com>
2017-08-20 14:05:31 +0200

    First commit.

---
+++ Tankz/Source/Tankz/TankzGameModeBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankzGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKZ_API ATankzGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+};
