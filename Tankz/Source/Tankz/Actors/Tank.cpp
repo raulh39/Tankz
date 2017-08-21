f329b74925f362310c8cdc22db559cc5cff70e6b
raul.huertas <raulh39@gmail.com>
2017-08-21 22:33:00 +0200

    Tank Actor has now a static mesh component

--- Tankz/Source/Tankz/Actors/Tank.cpp
+++ Tankz/Source/Tankz/Actors/Tank.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tank.h"
-
+#include "ConstructorHelpers.h"
+#include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
+#include "Engine.h"
 
 // Sets default values
 ATank::ATank()
@@ -9,6 +11,15 @@ ATank::ATank()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMeshComponent");
+
+	
+	auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Meshes/TankMesh.TankMesh'"));
+	if (MeshAsset.Object != nullptr) {
+		Mesh->SetStaticMesh(MeshAsset.Object);
+	} else {
+		GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, TEXT("Mesh Asset not found"));
+	}
 }
 
 // Called when the game starts or when spawned
--- Tankz/Source/Tankz/Actors/Tank.h
+++ Tankz/Source/Tankz/Actors/Tank.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	ATank();
 
+	UPROPERTY()
+	UStaticMeshComponent* Mesh;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
