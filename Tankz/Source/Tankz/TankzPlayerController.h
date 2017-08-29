7ed18a8d249a9454499df78e0bf9aa08f03852dc
raul.huertas <raulh39@gmail.com>
2017-08-29 16:14:13 +0200

    Created an empty C++ PlayerController. TankzGameModeBP will use it.

---
+++ Tankz/Source/Tankz/TankzPlayerController.h
@@ -0,0 +1,20 @@
+// No copyright notice because I don't like copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankzPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKZ_API ATankzPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
