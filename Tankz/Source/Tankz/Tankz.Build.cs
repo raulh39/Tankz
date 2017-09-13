c1bee70d4b730738d8dc8bd9d389d08e4543d284
raul.huertas <raulh39@gmail.com>
2017-09-13 16:41:44 +0200

    Adding boost directory to includes search paths

--- Tankz/Source/Tankz/Tankz.Build.cs
+++ Tankz/Source/Tankz/Tankz.Build.cs
@@ -19,5 +19,7 @@ public class Tankz : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+		PrivateIncludePaths.Add( "../../boost" );
 	}
 }
